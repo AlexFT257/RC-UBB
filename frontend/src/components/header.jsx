124b40463b96aa492bb9f38ddacb02c6ccc16564
AlexFT257 <ftb2570@gmail.com>
2023-04-09 16:34:24 -0400

    agregando el backend desde branch edison

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,28 +1,32 @@
+import Profile from "@/pages/profile";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Header() {
     return (
         <>
-        <header className="bg-slate-600 shadow-[#1c1b21] shadow-inner  to-[#1c1b21]  flex flex-row p-4 justify-evenly  items-center">
+        {/*  gradient-to-t from-[#E2E2E2]  to-[#B2B2B2] */}
+        <header className="bg-white text-black font-semibold shadow-xl mb-2   flex flex-row p-4 justify-evenly  items-center">
           {/* left side */}
-          <div className="flex flex-row gap-2 items-center">
+          <div className="flex-row gap-2 hidden md:flex items-center">
             <Image src="/31_minutos_logo.svg" alt="Icono" width={50} height={50}/>
             <h1>RC UBB</h1>
           </div>
           
           {/* Search bar */}
           <div className="flex flex-row gap-2 ml-auto ">
-            <input className="rounded-lg p-2 px-2 appearance-none outline-none focus:outline-2 focus:outline-slate-800" type="text" placeholder="Buscar" />
+            <input className="rounded-lg p-2 w-3/4 md:w-full appearance-none outline-none focus:outline-2 focus:outline-slate-800" type="text" placeholder="Buscar" />
           </div>
 
           
           {/* right side */}
-          <div className="flex flex-row gap-2 ml-auto mr-2">
-            <h1>Inicio</h1>
-            <h1>Grupos</h1>
-            <h1>Perfil</h1>
-            <h1>Salir</h1>
+          <div className="flex flex-row  gap-2 ml-auto mr-2">
+            <Link href="/" className="rounded-md m-2">Inicio</Link>
+            <Link href="/groupPage" className="rounded-md m-2">Grupos</Link>
+            <Link href="/profile" className="rounded-md m-2">Perfil</Link>
+            <h1 className="m-2">Salir</h1>
+            
           </div>
           
         </header>
--- frontend/src/components/recentGroups.jsx
+++ frontend/src/components/recentGroups.jsx
@@ -3,32 +3,32 @@ import Image from "next/image";
 export default function RecentGroups() {
   return (
     <>
-      <div className="m-2 flex flex-col gap-2 ">
+      <div className="m-2 flex flex-col gap-2  rounded-md p-2">
         <h1 className="text-lg font-semibold">Grupos recientes</h1>
         <div className="m-2 ml-4 flex flex-col gap-2">
-          <div className="rounded-md bg-[url('/31-minutos-banner.jpg')]  bg-cover">
-            <div className="flex items-center gap-2  backdrop-blur-sm rounded-md overflow-hidden p-2 ">
+          <button className="rounded-md hover:bg-[#D3D3D3] active:bg-[#C3C3C3] ">
+            <div className="flex items-center gap-2   rounded-md overflow-hidden p-2 ">
               <Image
                 src="/31_minutos_logo.svg"
                 alt="Icono"
                 width={25}
                 height={25}
               />
-              <h1 className="text-black font-semibold text-lg">31 Minutos</h1>
+              <h1 className="text- font-semibold text-lg">31 Minutos</h1>
             </div>
-          </div>
+          </button>
 
-          <div className="rounded-md bg-[url('/31-minutos-banner.jpg')]  bg-cover">
-            <div className="flex items-center gap-2  backdrop-blur-sm rounded-md overflow-hidden p-2 ">
+          <button className="rounded-md hover:bg-[#D3D3D3] active:bg-[#C3C3C3]   bg-cover">
+            <div className="flex items-center gap-2 rounded-md overflow-hidden p-2 ">
               <Image
                 src="/31_minutos_logo.svg"
                 alt="Icono"
                 width={25}
                 height={25}
               />
-              <h1 className="text-black font-semibold text-lg">31 Minutos</h1>
+              <h1 className="text- font-semibold text-lg">31 Minutos</h1>
             </div>
-          </div>
+          </button>
         </div>
       </div>
     </>
