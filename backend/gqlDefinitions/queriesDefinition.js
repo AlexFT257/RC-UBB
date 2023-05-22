53012edffa7a2bf4c2f14c5cb761fee5ac71fa13
NikoGB <114112855+NikoGB@users.noreply.github.com>
2023-05-22 18:19:06 -0400

    Add files via upload

---
+++ backend/gqlDefinitions/queriesDefinition.js
@@ -0,0 +1,58 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+
+type Query {
+    #Carrera
+    all_carreras: [Carrera]!
+    buscarCarrera(id: ID!): Carrera
+    #Chat
+    all_chats: [Chat]!
+    buscarChat(id: ID!): Chat
+    #Grupo
+    all_grupos: [Grupo]!
+    buscarGrupo(buscar: String!): [Grupo]
+    buscarGrupoId(id: ID!): Grupo
+    buscarGrupoUsuario(usuario: ID!): [Grupo]
+    buscarGrupoAdmin(admin: ID!): [Grupo]
+    #Mensaje
+    all_mensajes: [Mensaje]!
+    buscarMensaje(buscar: String!): [Mensaje]
+    buscarMensajeId(id: ID!): Mensaje
+    buscarMensajeUsuario(usuario: ID!): [Mensaje]
+    buscarMensajeHora(hora: Date!): [Mensaje]
+    #Opcion
+    all_opciones: [Opcion]!
+    buscarOpcion(buscar: String!): [Opcion]
+    buscarOpcionId(id: ID!): Opcion
+    buscarOpcionUsuario(usuario: ID!): [Opcion]
+    #Publicacion
+    all_publicaciones: [Publicacion]!
+    buscarPublicacion(buscar: String!): [Publicacion]
+    buscarPublicacionId(id: ID!): Publicacion
+    buscarPublicacionUsuario(usuario: ID!): [Publicacion]
+    buscarPublicacionHora(hora: Date!): [Publicacion]
+    #Usuario
+    all_usuarios: [Usuario]!
+    buscarUsuario(buscar: String!): [Usuario]
+    buscarUsuarioId(id: ID!): Usuario
+    buscarUsuarioCorreo(correo: String!): [Usuario]
+    buscarUsuarioFechaNacimiento(fecha_nacimiento: Date!): [Usuario]
+    buscarUsuarioCarrera(carrera: ID!): [Usuario]
+    buscarUsuarioEstado(estado: String!): [Usuario]
+    buscarUsuarioRol(rol: String!): [Usuario]
+    cantidadAmigosUsuario(id: ID!): Int!
+    cantidadPublicacionesUsuario(id: ID!): Int!
+    cantidadComentariosUsuario(id: ID!): Int!
+    cantidadMeGustaUsuario(id: ID!): Int!
+    #Votacion
+    all_votaciones: [Votacion]!
+    buscarVotacion(buscar: String!): [Votacion]
+    buscarVotacionId(id: ID!): Votacion
+    buscarVotacionUsuario(usuario: ID!): [Usuario]
+    buscarVotacionPublicacion(publicacion: ID!): [Publicacion]
+    #buscarVotacionResultados(id: ID!): [Votacion]
+}
+`
+
+module.exports = typeDefs;
